efc3c4d83fbd9603b8c305bbb902be4d2ac5cae2
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2023-09-16 12:07:32 +0100

    Add static_assert
    Added a crude but effective static_assertion tool

---
+++ static_assert.h
@@ -0,0 +1,10 @@
+namespace dp{
+    template<bool Condition>
+    struct static_assert_98{};
+
+    //If you get an error pointing here saying that you're
+    //trying to use an incomplete type, your assertion failed.
+    //Address that, not this. This doesn't need fixing.
+    template<>
+    struct static_assert_98<false>;
+}
