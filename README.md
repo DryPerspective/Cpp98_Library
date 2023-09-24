22ccd0d328111adb489ba14524e64164b4e7431e
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2023-09-24 11:45:34 +0100

    Added byte

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A collection of tools which represent a large chunk of the modern C++ standard library, written in ISO C++98, and compatible with all future C++ standards*. While I cannot recreate features which rely on core language features, much of the STL is implementable in C++98; though my general recommendation is to use an official stdlib distribution over this if you are on a modern C++ standard which has access to those features.
 Created because I spend a fair bit of time maintaining and adapting legacy code which for various reasons cannot be updated to modern C++.
 
-This is a header-only library. The include directory should be added to your project's include path, with the cpp98 subdirectory containing the headers which should see the outside world and and the bits subdirectory following the gcc pattern of being internal files for the library.
+This is a header-only library. The include directory should be added to your project's include path. There are three subdirectories to the include directory: `cpp98`, `bits`, and `borland`. The `cpp98` subdirectory is where the headers which you should include in your code live, and where the headers listed below can be found. The `bits` subdirectory is for files used in the internal workings of the library, liable to change at my discretion. Don't include them. The `borland` subdirectory contains a single header used to manipulate Borland string types.
 
 The code is designed to emulate the standard library as closely as is reasonable in this language standard. For example, if you want to use a std::array-style array, you would `#include "cpp98/array.h"` and construct it via `dp::array<foo,size> myarray = {....};`
 
@@ -15,6 +15,7 @@ A writeup of all the features can be found on [the project's wiki](https://githu
 * algorithm
 * array
 * bit
+* byte
 * expected 
 * iterator
 * memory
---
+++ include/cpp98/byte.h
@@ -0,0 +1,103 @@
+#ifndef DP_CPP98_BYTE
+#define DP_CPP98_BYTE
+
+#include "bits/static_assert_no_macro.h"
+
+#include "cpp98/type_traits.h"
+
+/*
+*	Implementing a byte is tricky.
+*	There are no scoped enums we can use, as the standard does, and we can't use an unscoped enum as that's no better than a typedef for unsigned char.
+*	If we're asserting that a byte is exactly one byte in size, we can't inherit from a static_assert class because at the point we're asserting the size of an
+*	incomplete type; we can't composite because that would add size to the overall struct (and on one compiler this was tested on, empty classes were padded
+*   up to a size of 8 bytes (no clue why), so that just leaves us with the option of a free floating assert. Which is clunky but will work.
+*/
+
+
+
+
+namespace dp {
+
+
+	class byte {
+		unsigned char val;
+
+		//We want to encapsulate val properly, so we unfortunately have to friend up every function which should be able to manipulate it directly.
+		template<typename IntType>
+		friend typename dp::enable_if<dp::is_integral<IntType>::value, IntType>::type to_integer(const byte&);
+
+		template<typename IntType>
+		friend typename dp::enable_if<dp::is_integral<IntType>::value, dp::byte&>::type operator<<=(dp::byte& b, IntType in);
+
+		template<typename IntType>
+		friend typename dp::enable_if<dp::is_integral<IntType>::value, dp::byte&>::type operator<<=(dp::byte& b, IntType in);
+
+		friend inline dp::byte& operator|=(dp::byte& lhs, dp::byte rhs);
+		friend inline dp::byte& operator&=(dp::byte& lhs, dp::byte rhs);
+		friend inline dp::byte& operator^=(dp::byte& lhs, dp::byte rhs);
+
+	public:
+		explicit byte() : val(0) {}
+		explicit byte(unsigned char in) : val(in) {}	//To match C++17 relaxed enum initialization rules
+	};
+
+	//I don't like this approach either, but I dohn't have a lot of options
+	//And to be honest, odds are this will never be a problem as almost all systems will have byte be one byte in size.
+	static const dp::static_assert_98<sizeof(dp::byte) == 1> dp_assert_byte_size;
+
+	template<typename IntType>
+	typename dp::enable_if<dp::is_integral<IntType>::value, IntType>::type to_integer(const dp::byte& in) {
+		return in.val;
+	}
+
+	template<typename IntType>
+	typename dp::enable_if<dp::is_integral<IntType>::value, dp::byte&>::type operator<<=(dp::byte& b, IntType in) {
+		b.val <<= in;
+		return b;
+	}
+
+	template<typename IntType>
+	typename dp::enable_if<dp::is_integral<IntType>::value, dp::byte&>::type operator>>=(dp::byte& b, IntType in) {
+		b.val >>= in;
+		return b;
+	}
+
+	template<typename IntType>
+	typename dp::enable_if<dp::is_integral<IntType>::value, dp::byte>::type operator<<(dp::byte b, IntType in) {
+		return b <<= in;
+	}
+
+	template<typename IntType>
+	typename dp::enable_if<dp::is_integral<IntType>::value, dp::byte>::type operator>>(dp::byte b, IntType in) {
+		return b >>= in;
+	}
+
+	inline dp::byte& operator|=(dp::byte& lhs, dp::byte rhs) {
+		lhs.val |= rhs.val;
+		return lhs;
+	}
+	inline dp::byte operator|(dp::byte lhs, dp::byte rhs) {
+		return lhs |= rhs;
+	}
+
+	inline dp::byte& operator&=(dp::byte& lhs, dp::byte rhs) {
+		lhs.val &= rhs.val;
+		return lhs;
+	}
+	inline dp::byte operator&(dp::byte lhs, dp::byte rhs) {
+		return lhs &= rhs;
+	}
+
+	inline dp::byte& operator^=(dp::byte& lhs, dp::byte rhs) {
+		lhs.val ^= rhs.val;
+		return lhs;
+	}
+	inline dp::byte operator^(dp::byte lhs, dp::byte rhs) {
+		return lhs ^= rhs;
+	}
+
+}
+
+
+
+#endif
