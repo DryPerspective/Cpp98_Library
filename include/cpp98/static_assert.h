183f12562eda746e5f8a97efa22a5d2a1732d4f9
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2023-10-03 09:13:45 +0100

    Changed static_assert macro to allow commas in parameters, and made a minor tweak to some Borland-specific code for span

--- include/cpp98/static_assert.h
+++ include/cpp98/static_assert.h
@@ -4,7 +4,8 @@
 #include "bits/static_assert_no_macro.h"
 
 #if !defined STATIC_ASSERT && !defined DP_NO_ASSERT_MACRO
-#define STATIC_ASSERT(args) dp::static_assert_98<args>();
+//#define STATIC_ASSERT(args) dp::static_assert_98<args>();
+#define STATIC_ASSERT(...) dp::static_assert_98<__VA_ARGS__>();
 #endif
 
 
