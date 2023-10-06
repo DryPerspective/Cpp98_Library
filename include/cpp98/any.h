93ae1391cde6988429c49cc5a853c74ae3942a77
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2023-10-06 13:52:03 +0100

    Added small storage optimization to any

--- include/cpp98/any.h
+++ include/cpp98/any.h
@@ -5,10 +5,14 @@
 #include <stdexcept>
 
 #include "cpp98/type_traits.h"
+#include "bits/unbound_storage.h"
 
-#ifdef DP_BORLANd
+#ifdef DP_BORLAND
 #include "bits/type_traits_ns.h"
 #endif
+
+//I know how much space will be optimal in my own internal implementation of unbound_storage.
+#define DP_ANY_MAX_STACK_SIZE sizeof(long double)
 
 namespace dp {
 
@@ -48,13 +52,17 @@ namespace dp {
 
 
 	class any {
+
+		typedef dp::unbound_storage<DP_ANY_MAX_STACK_SIZE> storage_type;
+
 		//Operation performed by our manager function
 		struct op {
 			enum type {
 				access,
 				get_type_info,
 				clone,
-				destroy
+				destroy,
+				transfer
 			};
 		};
 
@@ -65,18 +73,11 @@ namespace dp {
 			any* m_any;
 		};
 
-		//Pointer to the resource. Can't scoped/unique_ptr<void> and shared_ptr adds unnecessary overhead
-		void* m_storage;
-
-		//Our magic manager which tracks everything for us.
-		typedef void(*manager_ptr)(op::type, const any*, arg*);
-		manager_ptr m_manager;
-
 		//Templated manager which "magically" tracks the internal type for us.
 		template<typename T>
-		struct manager {
+		struct manager_heap {
 			static void manage(op::type inOp, const any* inAny, arg* inArg) {
-				const T* ptr = static_cast<const T*>(inAny->m_storage);
+				const T* ptr = static_cast<const T*>(inAny->m_storage.template get<void*>());
 				switch (inOp) {
 				case op::access:
 					inArg->m_object = const_cast<T*>(ptr);
@@ -85,28 +86,90 @@ namespace dp {
 					inArg->m_info = &typeid(T);
 					break;
 				case op::clone:
-					inArg->m_any->m_storage = new T(*ptr);
+					inArg->m_any->m_storage.template get<void*>() = new T(*ptr);
 					inArg->m_any->m_manager = inAny->m_manager;
 					break;
 				case op::destroy:
 					delete ptr;
 					break;
+				case op::transfer:
+					inArg->m_any->m_storage.template assign<void*>(inAny->m_storage.template get<void*>());
+					inArg->m_any->m_manager = inAny->m_manager;
+					const_cast<any*>(inAny)->m_manager = NULL;
 				}
 			}
 
 			template<typename ValT>
-			static void create(void*& inPtr, const ValT& val) {
-				inPtr = new T(val);
+			static void create(storage_type& inStorage, const ValT& val) {
+				inStorage.template get<void*>() = new T(val);
 			}
+
+			static T* access(const storage_type& in) {
+				return static_cast<T*>(in.get<void*>());
+			}
+		};
+
+		template<typename T>
+		struct manager_stack {
+			static void manage(op::type inOp, const any* inAny, arg* inArg) {
+				const T* ptr = static_cast<const T*>(&inAny->m_storage.template get<T>());
+				switch (inOp) {
+				case op::access:
+					inArg->m_object = const_cast<T*>(ptr);
+					break;
+				case op::get_type_info:
+					inArg->m_info = &typeid(T);
+					break;
+				case op::clone:
+					inArg->m_any->m_storage.template construct<T>(*ptr);
+					inArg->m_any->m_manager = inAny->m_manager;
+					break;
+				case op::destroy:
+					ptr->~T();
+					break;
+				case op::transfer:
+					inArg->m_any->m_storage.template construct<T>(*ptr);
+					ptr->~T();
+					inArg->m_any->m_manager = inAny->m_manager;
+					const_cast<any*>(inAny)->m_manager = NULL;
+					break;
+				}
+
+			}
+
+			template<typename ValT>
+			static void create(storage_type& inStorage, const ValT& val) {
+				inStorage.template construct<T>(val);
+			}
+
+			static T* access(const storage_type& in) {
+				const void* buff = &in.get<T>();
+				return static_cast<T*>(const_cast<void*>(buff));
+			}
+		};
+
+		//Metafunction to get the correct manager type for a given value type.
+		template<typename ValueType>
+		struct get_manager_type {
+			typedef typename dp::detail::valid_any_type<ValueType>::decay_type decay_type;
+			typedef typename dp::conditional<(sizeof(decay_type) > DP_ANY_MAX_STACK_SIZE), manager_heap<ValueType>, manager_stack<ValueType> >::type type;
 		};
 
 
 		template<typename ValueType>
 		friend void* dp::detail::any_caster(const any* in);
 
+		//Small storage optimization for the held object
+		storage_type m_storage;
+
+		//Our magic manager which tracks everything for us.
+		typedef void(*manager_ptr)(op::type, const any*, arg*);
+		manager_ptr m_manager;
+
 	public:
 
-		any() : m_storage(NULL), m_manager(NULL) {}
+		any() : m_storage(), m_manager(NULL) {}
+
 		any(const any& other) {
 			if (other.has_value()) {
 				arg newArg;
@@ -115,12 +178,13 @@ namespace dp {
 			}
 			else {
 				m_manager = NULL;
-				m_storage = NULL;
+				m_storage.construct<void*>((void*)NULL);
 			}
 		}
 		template<typename ValueType>
-		any(const ValueType& value, typename dp::enable_if<dp::detail::valid_any_type<ValueType>::value, bool>::type = true) : m_manager(&manager<typename dp::detail::valid_any_type<ValueType>::decay_type>::manage) {
-			typedef manager<typename dp::detail::valid_any_type<ValueType>::decay_type> man;
+		any(const ValueType& value, typename dp::enable_if<dp::detail::valid_any_type<ValueType>::value, bool>::type = true) {//: m_manager(&typename get_manager_type<ValueType>::type::manage) {
+			typedef typename get_manager_type<ValueType>::type man;
+			m_manager = &man::manage;
 			man::create(m_storage, value);			
 		}
 
@@ -128,6 +192,7 @@ namespace dp {
 			this->reset();
 		}
 
+		
 		any& operator=(const any& other) {
 			any copy(other);
 			this->swap(copy);
@@ -140,18 +205,41 @@ namespace dp {
 			this->swap(copy);
 			return *this;
 		}
+		
+		void swap(any& other) {
+			//Can't do a flat swap as the anys may contain different types
+			if (!this->has_value() && !other.has_value()) return;
+			if (this->has_value() && other.has_value()) {
+				if (this == &other) return;
 
+				//Convoluted form of temp = A, A = B, B = temp
+				any temp;
+				arg temp_arg;
+				//Transfer other to temp
+				temp_arg.m_any = &temp;
+				other.m_manager(op::transfer, &other, &temp_arg);
+				//Transfer this to other
+				temp_arg.m_any = &other;
+				this->m_manager(op::transfer, this, &temp_arg);
+				//Transfer temp to this
+				temp_arg.m_any = this;
+				temp.m_manager(op::transfer, &temp, &temp_arg);
+			}
+			else {
+				any* empty = this->has_value() ? &other : this;
+				any* full = this->has_value() ? this : &other;
+				arg temp_arg;
+				temp_arg.m_any = empty;
+				full->m_manager(op::transfer, full, &temp_arg);
+			}
+					
+		}
+		
 		void reset() {
 			if (this->has_value()) {
 				m_manager(op::destroy, this, NULL);
 				m_manager = NULL;
 			}
-		}
-
-		void swap(any& other) {
-			using std::swap;
-			swap(m_storage, other.m_storage);
-			swap(m_manager, other.m_manager);		
 		}
 
 		bool has_value() const {
@@ -188,7 +276,9 @@ namespace dp {
 		void* any_caster(const dp::any* in) {
 			typedef typename dp::remove_cv<ValueType>::type base_type;
 			//Compare address first to hopefully avoid RTTI from typeid
-			if (in->m_manager == &any::manager<base_type>::manage || in->type() == typeid(ValueType)) return in->m_storage;
+			if (in->m_manager == &any::get_manager_type<base_type>::type::manage || in->type() == typeid(ValueType)) {
+				return any::get_manager_type<ValueType>::type::access(in->m_storage);
+			}
 			return NULL;
 		}
 	}
@@ -220,5 +310,7 @@ namespace dp {
 
 
 }
+
+#undef DP_ANY_MAX_STACK_SIZE
 
 #endif
