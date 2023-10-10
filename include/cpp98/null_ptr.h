36f7069ef84cf25551b6c8f46f527a9390b98b28
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2023-10-10 10:42:49 +0100

    Minor tweaks

--- include/cpp98/null_ptr.h
+++ include/cpp98/null_ptr.h
@@ -1,11 +1,15 @@
 #ifndef DP_CPP98_NULL_PTR
 #define DP_CPP98_NULL_PTR
 
+
+/*
+*  As with static_assert, we can't replicate a core language feature fully, nor can we use the same spelling; as this creates a warning on many compilers in C++98 mode
+*/
 namespace dp {
 	struct null_ptr_t {
 
 		template<typename T>
-		operator T* () const {
+		operator T* () const volatile {
 			return NULL;
 		}
 	};
